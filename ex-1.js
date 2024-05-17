2f43e0a27635a8bbe183ecac652e93126ba5de3a
LAPTOP-F25A12VH\Dear <natthaphong.ea@gmail.com>
2024-05-17 15:30:37 +0700

    feat(js): add answer ex1-2

--- ex-1.js
+++ ex-1.js
@@ -2,6 +2,17 @@
 
 function forEach(array, operation) {
   // Start coding here
+  return function (x) {
+    for (let i = 0; i < array.length; i++) {
+      newEmployeeSalaries.push(operation(array[i], x));
+    }
+    return newEmployeeSalaries;
+  };
+}
+
+function plusSalaries(salary, x) {
+  let newSalary = salary + x;
+  return newSalary;
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
@@ -9,4 +20,8 @@ const newEmployeeSalaries = [];
 
 // Using `forEach` function here
 
-console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
+const calculateSalary = forEach(employeeSalaries, plusSalaries);
+
+// forEach(employeeSalaries, plusSalaries);
+
+console.log(calculateSalary(5000)); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,27 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  // let result = 0;
+  let passScores = [];
+  for (let i = 0; i < array.length; i++) {
+    if (operation(array[i])) {
+      passScores.push(array[i]);
+    }
+    // result += operation(array[i]);
+  }
+
+  // if (result >= 5) {
+  if (passScores.length >= 5) {
+    return 'ผ่านเกณฑ์ ✅';
+  } else {
+    return 'ไม่ผ่านเกณฑ์ ❌';
+  }
+}
+
+function checkScore(score) {
+  if (score > 70) {
+    return score;
+  }
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -13,3 +34,11 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 let scoreRoom1Result;
 let scoreRoom2Result;
 let scoreRoom3Result;
+
+scoreRoom1Result = atLeastFive(studentScoresRoom1, checkScore);
+scoreRoom2Result = atLeastFive(studentScoresRoom2, checkScore);
+scoreRoom3Result = atLeastFive(studentScoresRoom3, checkScore);
+
+console.log(`นักเรียนห้องที่ 1 ${scoreRoom1Result}`);
+console.log(`นักเรียนห้องที่ 2 ${scoreRoom2Result}`);
+console.log(`นักเรียนห้องที่ 3 ${scoreRoom3Result}`);
